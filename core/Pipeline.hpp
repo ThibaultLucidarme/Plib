10d0b61605226c4b4618ae7e378a8e7f66403414
unknown <523138@NRM-U523138L002.ad.seagate.com>
2016-01-06 16:48:07 -0600

    added multithreading support

--- core/Pipeline.hpp
+++ core/Pipeline.hpp
@@ -107,6 +107,7 @@ namespace p
 			<< std::endl
 			<< e.what()
 			<< std::endl;
+			throw e;
 		}
 
 	}
@@ -115,19 +116,56 @@ namespace p
 	template<class I, class O>
 	void Pipeline<I,O>::RetroUpdate(Pipeline* input)
 	{
-		this->BackPropagate(input);
-		this->CheckPipelineConnection(input);
+		try
+		{
+			this->BackPropagate(input);
+			this->CheckPipelineConnection(input);
+		}
+		catch (std::exception& e)
+		{
+			std::cerr<< "Could not backPropagate Pipeline: '"
+			<< input->_name << "' from '" << _name <<"'"
+			<< std::endl
+			<< e.what()
+			<< std::endl;
+			throw e;
+		}
 	}
-
-	#ifndef __thread__
 
 	template<class I, class O>
 	void Pipeline<I,O>::Update(void)
 	{
 
-		for_each (_inputPipeline.begin(),
-					_inputPipeline.end(),
-					std::bind(&Pipeline::RetroUpdate,this,std::placeholders::_1));
+		#ifdef __thread__
+
+			static std::vector<std:unique_ptr<Thread>> threads;
+			if(Pipeline::useThreads)
+			{
+				// execute each previous pipeline in a separate thread
+				for_each (std::next(_inputPipeline.begin(),1),
+						_inputPipeline.end(),
+						[this](Pipeline* input){
+							std:unique_ptr<Thread> t = new Thread(std::bind(&Pipeline::RetroUpdate,this,std::placeholders::_1));
+							threads.push_back(t);
+						});
+
+				// join all threads before continuing execution
+				for_each (threads.begin(),
+						threads.end(),
+						[this](std::unique_ptr<Thread> t){t.join();})
+				threads.clear();
+			}
+			else for_each (_inputPipeline.begin(),
+						_inputPipeline.end(),
+						std::bind(&Pipeline::RetroUpdate,this,std::placeholders::_1));
+
+		#else
+
+			for_each (_inputPipeline.begin(),
+						_inputPipeline.end(),
+						bind(&Pipeline::RetroUpdate,this,std::placeholders::_1));
+
+		#endif
 
 		// Execute processing from input list
 		std::cout<<"Executing :'"<< _name <<"'"<<std::endl;
@@ -143,52 +181,9 @@ namespace p
 			<< std::endl
 			<< e.what()
 			<< std::endl;
+			throw e;
 		}
 	}
-
-	#else
-
-
-	template<class I, class O>
-	void Pipeline<I,O>::Update(void)
-	{
-		static std::vector<std:unique_ptr<Thread>> threads;
-
-		if(Pipeline::useThreads)
-		{
-			for_each (std::next(_inputPipeline.begin(),1),
-					_inputPipeline.end(),
-					[this](Pipeline* input){
-						std:unique_ptr<Thread> t = new Thread(std::bind(&Pipeline::RetroUpdate,this,std::placeholders::_1));
-						threads.push_back(t);
-					});
-
-			for_each (threads.begin(),
-					threads.end(),
-					[this](std::unique_ptr<Thread> t){t.join();})
-			threads.clear();
-		}
-		else for_each (_inputPipeline.begin(),
-					_inputPipeline.end(),
-					RetroUpdate);
-
-		// Execute processing from input list
-		std::cout<<"Executing :'"<< _name <<"'"<<std::endl;
-		try
-		{
-			_output = Execute( _input );
-			_isCalculated = true;
-		}
-		catch(std::exception& e)
-		{
-			std::cerr<< "Could not connect Execute: '"
-			<< _name <<"'"
-			<< std::endl
-			<< e.what()
-			<< std::endl;
-		}
-	}
-	#endif
 
 	template<class I, class O>
 	Pipeline<I,O>* Pipeline<I,O>::SetInput( Pipeline* p)
@@ -221,3 +216,71 @@ namespace p
 }
 
 #endif
+
+
+
+/******************************************************************************
+
+Example
+
+/******************************************************************************
+
+// #include "Thread.hpp"
+#include "Pipeline.hpp"
+
+using namespace std;
+using namespace p;
+
+
+class FilterA : public Pipeline<string,string>
+{
+	private:
+		string Execute(vector<string> inputs)
+		{
+			string out="";
+			vector<string>::iterator it = inputs.begin();
+			while(it < inputs.end()) {out+=(*it);it++;}
+			return out;
+		}
+	public:
+		FilterA(string s):Pipeline(s){}
+};
+
+class FilterB : public Pipeline<string,string>
+{
+	private:
+		string Execute(vector<string> inputs)
+		{
+			string out="";
+			vector<string>::iterator it = inputs.begin();
+			while(it < inputs.end()) {out+=(*it);it++;}
+			return out;
+		}
+	public:
+		FilterB(string s):Pipeline(s){}
+};
+
+int main( int argc, char** argv)
+{
+	FilterA* f1 = new FilterA("filter 1");
+	FilterB* f2 = new FilterB("filter 2");
+
+	f1->SetInput("f1i1")->SetInput("f1i2");
+	f2->SetInput(f1)->SetInput("f2i1");
+
+	f1->Update();
+
+	cout<<"caca"<<endl;
+
+	f2->Update();
+
+	cout<<(*f1)<<endl<<f2<<endl;
+
+
+	delete f1,f2;
+	return EXIT_SUCCESS;
+}
+
+
+
+*/
--- core/ProgressBar.hpp
+++ core/ProgressBar.hpp
@@ -9,47 +9,41 @@ class ProgressBar
 	int _size;
 
 	public:
-	ProgressBar( int* i, int* m)
+
+	ProgressBar( int* i, int* m, int s=50)
 	{
 		_current = i;
 		_max = m;
-		_size = 50;
+		_size = s>0?s:50;
 	}
-	
-	ProgressBar( int* i, int* m, int s)
-	{
-		_current = i;
-		_max = m;
-		_size = s;
-	}
-	
+
 	~ProgressBar()
 	{
 		_current = nullptr;
 		_max = nullptr;
 	}
-	
+
 	void SetSize(int s)
 	{
 		_size = s;
 	}
-	
+
 	void Progress()
 	{
 		int iter = *_current
 		int maxIter = *_max;
-		
+
 		int percentage = 100*iter/maxIter;
 		int scaledPercentage = _size*iter/maxIter;
-		
+
 		cout<<"\r[";
 		for(int i=0;i<=scaledPercentage;i++) cout<<"*";
 		for(int i=scaledPercentage+1;i<_size;i++) cout<<" ";
 		cout<<"]\t"<< iter <<"/"<<maxIter<<"\t("<<percentage<<"%)"<<flush;
-		
+
 		if(percentage==100)
 		std::cout<<std::endl;
-		
+
 	}
 
 
